2adc7b5bc6d89fbc67e553dac1a79a23a2b9c798
Stanislav Donetc <123019654+DonStMi@users.noreply.github.com>
2023-10-11 15:01:50 +0300

    create a todo list

--- app/src/main/java/ua/com/standiapp/MainActivity.kt
+++ app/src/main/java/ua/com/standiapp/MainActivity.kt
@@ -2,19 +2,41 @@ package ua.com.standiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
+import android.widget.ListView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
-    val label: TextView = findViewById<TextView>(R.id.main_lable)
-    val userData: EditText = findViewById(R.id.user_data)
-    val button: Button = findViewById(R.id.button)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val listView = findViewById<ListView>(R.id.listView)
+        val userData: EditText = findViewById(R.id.user_data)
+        val button: Button = findViewById(R.id.button)
+
+        val todos: MutableList<String> = mutableListOf()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, todos)
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener {adapterView, view, i, l ->
+            val text = listView.getItemAtPosition(i).toString()
+            adapter.remove(text)
+
+            Toast.makeText(this, "We've already remove : $text", Toast.LENGTH_LONG).show()
+
+        }
+
+
+        button.setOnClickListener{
+            val text  = userData.text.toString().trim()
+            if (text != "")
+                adapter.insert(text, 0)
+        }
 
 
     }
